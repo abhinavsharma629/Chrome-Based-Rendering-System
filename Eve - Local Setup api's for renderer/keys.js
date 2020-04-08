d058d18421f095d6dd5e6654e6903e5769293d1c
Abhinav Sharma <abhinavsharma629@gmail.com>
2020-04-08 13:38:19 +0530

    updates

--- Eve - Local Setup api's for renderer/keys.js
+++ Eve - Local Setup api's for renderer/keys.js
@@ -1,7 +1,7 @@
 //Keys and Uris
 module.exports = {
   mongoURI:
-    "mongodb+srv://abhinavsharma629:abhi629@@@onswitch-trkai.mongodb.net/test?retryWrites=true&w=majority",
+    "mongodb+srv://<username>:<password>@@@onswitch-trkai.mongodb.net/test?retryWrites=true&w=majority",
   PLAYLIST_DOWNLOAD_DIRECTORY:
     "C:/Users/User/Desktop/Embereon Assignment/embereon_adam/public/assets/"
 };
